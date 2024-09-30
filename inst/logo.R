b84c16b04f1970b29ab3691a1225367561b643b1
Hugo Marthinet <h.marthinet@gmail.com>
2024-09-30 16:00:28 +0200

    Update logo.R

--- inst/logo.R
+++ inst/logo.R
@@ -1,25 +1,25 @@
 library(hexSticker)
-        
-        
+
+
 #### Pour enlever le fond d'une image
 # https://www.remove.bg/fr
 
 
 # Create the sticker
 sticker(
-  subplot = "C:/Users/MARTHINETEX/Downloads/epiplus-main/360_F_182222479_ltyLPNCmQcAuSTnXHNJONIdg8FvA97Bk-removebg-preview.png",
-  filename = "epiplus_sticker.png",  # Output file name
+  subplot = "inst/image.png",
+  filename = "inst/logo.png",  # Output file name
   #img = "C:/Users/MARTHINETEX/Documents/02 - Code/logo.png",     # Path to your background image
-  package = "epiplus",                 # Package name
-  p_size = 16,
-  p_y = 1.23,
-  p_x = 1.4,# Text size
-  s_x = 0.85,                            # X position of the image
-  s_y = 1,                         # Y position of the image
-  s_width = 0.75,                      # Width of the image
+  package = "dataDesc",                 # Package name
+  p_size = 26,
+  p_y = 1.5,
+  p_x = 1,# Text size
+  s_x = 1,                            # X position of the image
+  s_y = 0.88,                         # Y position of the image
+  s_width = 0.65,                      # Width of the image
   h_fill = "#FFFFFF",                 # Background color
-  h_color = "#1F77B4",                # Hex border color
-  p_color = "#1F77B4",                # Package name color
+  h_color = "orange2",                # Hex border color
+  p_color = "orange2",                # Package name color
   layout = "straight",                # Text layout
   text_y = 1.2                       # Y position of the text
-)
+) 
