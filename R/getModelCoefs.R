4bd104692e742898796f0d1508da85e9bf41ff85
Hugo Marthinet <h.marthinet@gmail.com>
2024-12-13 09:18:35 +0100

    Update getModelCoefs.R

--- R/getModelCoefs.R
+++ R/getModelCoefs.R
@@ -1,34 +1,46 @@
 getModelCoefs <- function(
-    model, # Une sortie de modèle avec un attribut 'coefficients'
-    coefs_digits = 2,
-    p_digits = 3,
-    p_threshold = 0.001,
-    merge_p = TRUE
-    ) {
+  model, # Une sortie de modèle avec un attribut 'coefficients'
+  coefs_digits = 2,
+  p_digits = 3,
+  p_threshold = 0.001,
+  merge_CI = TRUE,
+  merge_p = TRUE
+) {
   
   coefficients <- summary(model)$coefficients[, 1]
   se <- summary(model)$coefficients[, 2]
   
   if (merge_p) {
-    res <- data.frame(
-      Variable = rownames(summary(model)$coefficients)[-1],
-      Coefs = paste0(formatC(exp(coefficients)[-1], digits = coefs_digits, format = "f"), " [",     # OR
-                     formatC(exp(coefficients - 1.96 * se)[-1], digits = coefs_digits, format = "f"), "-",      # IC inf
-                     formatC(exp(coefficients + 1.96 * se)[-1], digits = coefs_digits, format = "f"), "], p ", # IC sup
-                     ifelse(summary(model)$coefficients[-1, 4] < p_threshold,
-                            paste("<", p_threshold),
-                            paste("=", formatC(summary(model)$coefficients[-1, 4], digits = p_digits, format = "f"))))      # p val
+      res <- data.frame(
+        Variable = rownames(summary(model)$coefficients)[-1],
+        Coefs = paste0(formatC(exp(coefficients)[-1], digits = coefs_digits, format = "f"), " [",     # OR
+                       formatC(exp(coefficients - 1.96 * se)[-1], digits = coefs_digits, format = "f"), "-",      # IC inf
+                       formatC(exp(coefficients + 1.96 * se)[-1], digits = coefs_digits, format = "f"), "], p ", # IC sup
+                       ifelse(summary(model)$coefficients[-1, 4] < p_threshold,
+                              paste("<", p_threshold),
+                              paste("=", formatC(summary(model)$coefficients[-1, 4], digits = p_digits, format = "f"))))      # p val
     )
   } else {
-    res <- data.frame(
-      Variable = rownames(summary(model)$coefficients)[-1],
-      Coefs = paste0(formatC(exp(coefficients)[-1], digits = coefs_digits, format = "f"), " [",     # OR
-                     formatC(exp(coefficients - 1.96 * se)[-1], digits = coefs_digits, format = "f"), "-",      # IC inf
-                     formatC(exp(coefficients + 1.96 * se)[-1], digits = coefs_digits, format = "f"), "]"),# IC sup
-      p = ifelse(summary(model)$coefficients[-1, 4] < p_threshold,
-                                 paste0("<", p_threshold),
-                                 formatC(summary(model)$coefficients[-1, 4], digits = p_digits, format = "f"))    # p val
+    if (merge_CI) {
+      res <- data.frame(
+        Variable = rownames(summary(model)$coefficients)[-1],
+        Coefs = paste0(formatC(exp(coefficients)[-1], digits = coefs_digits, format = "f"), " [",     # OR
+                       formatC(exp(coefficients - 1.96 * se)[-1], digits = coefs_digits, format = "f"), "-",      # IC inf
+                       formatC(exp(coefficients + 1.96 * se)[-1], digits = coefs_digits, format = "f"), "]"),# IC sup
+        p = ifelse(summary(model)$coefficients[-1, 4] < p_threshold,
+                   paste0("<", p_threshold),
+                   formatC(summary(model)$coefficients[-1, 4], digits = p_digits, format = "f")))    # p val
+    } else {
+      res <- data.frame(
+        Variable = rownames(summary(model)$coefficients)[-1],
+        Coefs = formatC(exp(coefficients)[-1], digits = coefs_digits, format = "f"),    # OR
+        CIlower = formatC(exp(coefficients - 1.96 * se)[-1], digits = coefs_digits, format = "f"),
+        CIupper = formatC(exp(coefficients + 1.96 * se)[-1], digits = coefs_digits, format = "f"),
+        p = ifelse(summary(model)$coefficients[-1, 4] < p_threshold,
+                   paste0("<", p_threshold),
+                   formatC(summary(model)$coefficients[-1, 4], digits = p_digits, format = "f"))    # p val
     )
+    }
   }
   res
 }
