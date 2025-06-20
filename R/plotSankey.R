be9f897dc25bd7f19dd63a23faf9d75602634f91
Hugo Marthinet <h.marthinet@gmail.com>
2025-06-20 16:59:47 +0200

    Update plotSankey.R

--- R/plotSankey.R
+++ R/plotSankey.R
@@ -1,7 +1,7 @@
 plotSankey <- function(Nodes, Links, 
-                       addLab = c("none", "n", "p")) {
+                            addLab = c("none", "n", "p")) {
   addLab <- match.arg(addLab, c("none", "n", "p"))
-
+  
   if (addLab != "none") {
     if(all(table(Links$source) == table(Links$source)[1])) {
       nrow_per_times <- table(Links$source)[1]^2
@@ -9,24 +9,24 @@ plotSankey <- function(Nodes, Links,
       for (i in 1:n_times) {
         tab <- Links[((i-1)*nrow_per_times+1):((i-1)*nrow_per_times+nrow_per_times), ]
         vals <- as.numeric(by(tab$value, tab$target, sum))
-          ifelse(addLab == "p",
-                 Nodes[Nodes$X %in% (unique(tab$target) + 1), "label"] <- paste0(
-                   Nodes[Nodes$X %in% (unique(tab$target) + 1), "label"], " (",
-                   formatC(vals/sum(vals)*100, digits = 1, format = 'f'), "%)"),
-                 Nodes[Nodes$X %in% (unique(tab$target) + 1), "label"] <- paste0(
-                   Nodes[Nodes$X %in% (unique(tab$target) + 1), "label"], " (N=",
-                   formatC(vals, digits = 0, format = "f", big.mark = " "), ")"))
+        ifelse(addLab == "p",
+               Nodes[Nodes$X %in% (unique(tab$target) + 1), "label"] <- paste0(
+                 Nodes[Nodes$X %in% (unique(tab$target) + 1), "label"], " (",
+                 formatC(vals/sum(vals)*100, digits = 1, format = 'f'), "%)"),
+               Nodes[Nodes$X %in% (unique(tab$target) + 1), "label"] <- paste0(
+                 Nodes[Nodes$X %in% (unique(tab$target) + 1), "label"], " (N=",
+                 formatC(vals, digits = 0, format = "f", big.mark = " "), ")"))
         
         if (i == 1) {
           vals <- as.numeric(by(tab$value, tab$source, sum))
-           
-            ifelse(addLab == "p",
-                   Nodes[Nodes$X %in% (unique(tab$source) + 1), "label"] <-paste0(
-                     Nodes[Nodes$X %in% (unique(tab$source) + 1), "label"], " (",
-                     formatC(vals/sum(vals)*100, digits = 1, format = 'f'), "%)"),
-                   Nodes[Nodes$X %in% (unique(tab$source) + 1), "label"] <-paste0(
-                     Nodes[Nodes$X %in% (unique(tab$source) + 1), "label"], " (N=",
-                     formatC(vals, digits = 0, format = "f", big.mark = " "), ")"))
+          
+          ifelse(addLab == "p",
+                 Nodes[Nodes$X %in% (unique(tab$source) + 1), "label"] <-paste0(
+                   Nodes[Nodes$X %in% (unique(tab$source) + 1), "label"], " (",
+                   formatC(vals/sum(vals)*100, digits = 1, format = 'f'), "%)"),
+                 Nodes[Nodes$X %in% (unique(tab$source) + 1), "label"] <-paste0(
+                   Nodes[Nodes$X %in% (unique(tab$source) + 1), "label"], " (N=",
+                   formatC(vals, digits = 0, format = "f", big.mark = " "), ")"))
         }
       }
     } else {
@@ -34,30 +34,33 @@ plotSankey <- function(Nodes, Links,
     }
   }
   
+  
   # Adding transparency to the links
   # Conversion to rgba character string
   Links$color <- apply(grDevices::col2rgb(Links$color), 2, function(x) {
     paste0("rgba(", x[1], ",", x[2], ",", x[3], ",0.4)")
   })
-    
+  
   fig <- plot_ly(
-      type = "sankey",
-      orientation = "h",
-      alpha_stroke = 0.2,
-      node = list(
-        label = Nodes$label,
-        color = Nodes$color,
-        pad = 15,
-        thickness = 20,
-        line = list(color = "black", width = 0.5)
-      ),
-      link = list(
-        source = Links$source,
-        target = Links$target,
-        value =  Links$value,
-        color = Links$color
-      )
+    type = "sankey",
+    orientation = "h",
+    alpha_stroke = 0.2,
+    node = list(
+      label = Nodes$label,
+      color = Nodes$color,
+      pad = 15,
+      thickness = 20,
+      line = list(color = "black", width = 0.5),
+      x = Nodes$x,
+      y = Nodes$y
+    ),
+    link = list(
+      source = Links$source,
+      target = Links$target,
+      value =  Links$value,
+      color = Links$color
     )
+  )
   fig <- fig %>% layout(font = list(size = 16, color = "black", weight = "bold"))
   fig
 }
